a860d3a57aaddf37433c41890e3404c7fa3063f4
IanPrice <ianadamprice@gmail.com>
2022-01-09 13:36:54 -0500

    Get response to first question to transfer to readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,42 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
 // TODO: Create an array of questions for user input
-//const questions = [];
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
+const questions = [{
+  type: 'input',
+  name: 'title',
+  message: 'What is the title of your project? (Required)',
+  //validate to make sure there is a value there
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter your project title.');
+      return false;
     }
-  ])
-  .then(answers => console.log(answers));
+  }
+}];
+
 
 // TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err)
+      throw err;
+      console.log('Success! README was succesfully created!')
+    });
+};
 // TODO: Create a function to initialize app
-//function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then(function (userInput) {
+      console.log(userInput)
+      writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
-//init();
+init();
 
